fe92f5877b52cb70eda1e47df40e92abb08bc45e
jerenz pellina <pellina01@gmail.com>
2020-11-16 10:18:24 +0800

    raw_input

--- simpleI2C.py
+++ simpleI2C.py
@@ -21,6 +21,7 @@ while True:
     byte = ConvertStringsToBytes(string)
     if string == "1":
         response = I2Cbus.read_i2c_block_data(I2C_SLAVE_ADDRESS, 0x00, 2)
-        print(''.join(response).decode('utf-8'))
+        # print(''.join(response).decode('utf-8'))
+        print(response)
     else:
         I2Cbus.write_i2c_block_data(I2C_SLAVE_ADDRESS, 0x00, byte)
