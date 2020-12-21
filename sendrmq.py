f197534759a0578ad154d528a377b3225f2a00d9
jerenz pellina <pellina01@gmail.com>
2020-12-21 20:06:02 +0800

    trial rabbitmq added

--- sendrmq.py
+++ sendrmq.py
@@ -7,13 +7,21 @@ channel = connection.channel()
 
 channel.queue_declare(queue='task_queue', durable=True)
 
-message = "Hello mama mo!"
-channel.basic_publish(
-    exchange='',
-    routing_key='task_queue',
-    body=message,
-    properties=pika.BasicProperties(
-        delivery_mode=2,  # make message persistent
-    ))
-print(" [x] Sent %r" % message)
-connection.close()
+
+def send_msg(msg):
+    message = msg
+    channel.basic_publish(
+        exchange='',
+        routing_key='task_queue',
+        body=message,
+        properties=pika.BasicProperties(
+            delivery_mode=2,  # make message persistent
+        ))
+    print(" [x] Sent %r" % message)
+
+
+while True:
+    msg = input('say something: ')
+    send_msg(msg)
+
+# connection.close()
